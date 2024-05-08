97f60d219af7652b5415d674baeaa0f5f95031fe
daebangstn <gunho1107@naver.com>
2024-05-08 20:58:54 +0900

    fit my env

--- learning/skill/model.py
+++ learning/skill/model.py
@@ -17,51 +17,14 @@ class SkillModel(StyleModel):
         def __init__(self, net, **kwargs):
             super().__init__(net, **kwargs)
 
-        def forward(self, input_dict):
-            normed_obs = input_dict['obs']
-            normed_obs_actor, normed_obs_critic = self.attach_latent(normed_obs, input_dict['latent'])
-            mu, logstd = self.actor_module(normed_obs_actor)
-            sigma = torch.exp(logstd)
-            distr = torch.distributions.Normal(mu, sigma, validate_args=False)
-
-            normalized_value = self.critic_module(normed_obs_critic)
-
-            result = {
-                'mus': mu,
-                'sigmas': sigma,
-                'rnn_states': None,
-            }
-
-            if input_dict['is_train']:
-                prev_neglogp = self.neglogp(input_dict['prev_actions'], mu, sigma, logstd)
-                result.update({
-                    'prev_neglogp': torch.squeeze(prev_neglogp),
-                    'entropy': distr.entropy().sum(dim=-1),
-                    'values': normalized_value,
-                    'rollout_disc_logit': self.disc(input_dict['normalized_rollout_disc_obs']),
-                    'replay_disc_logit': self.disc(input_dict['normalized_replay_disc_obs']),
-                    'demo_disc_logit': self.disc(input_dict['normalized_demo_disc_obs']),
-                    'enc': self.enc(input_dict['normalized_rollout_disc_obs']),
-                })
-            else:
-                selected_action = distr.sample()
-                neglogp = self.neglogp(selected_action, mu, sigma, logstd)
-                result.update({
-                    'neglogpacs': torch.squeeze(neglogp),
-                    'actions': selected_action,
-                    'values': self.denorm_value(normalized_value),
-                })
-            return result
-
         # def forward(self, input_dict):
         #     normed_obs = input_dict['obs']
-        #     ase_latents = input_dict['latent']
-        #     use_hidden_latents = input_dict.get('use_hidden_latents', True)
-        #
-        #     mu, logstd = self.a2c_network.eval_actor(normed_obs, ase_latents, use_hidden_latents)
+        #     normed_obs_actor, normed_obs_critic = self.attach_latent(normed_obs, input_dict['latent'])
+        #     mu, logstd = self.actor_module(normed_obs_actor)
         #     sigma = torch.exp(logstd)
         #     distr = torch.distributions.Normal(mu, sigma, validate_args=False)
-        #     normalized_value = self.a2c_network.eval_critic(normed_obs, ase_latents, use_hidden_latents)
+        #
+        #     normalized_value = self.critic_module(normed_obs_critic)
         #
         #     result = {
         #         'mus': mu,
@@ -75,10 +38,10 @@ class SkillModel(StyleModel):
         #             'prev_neglogp': torch.squeeze(prev_neglogp),
         #             'entropy': distr.entropy().sum(dim=-1),
         #             'values': normalized_value,
-        #             'rollout_disc_logit': self.a2c_network.eval_disc(input_dict['normalized_rollout_disc_obs']),
-        #             'replay_disc_logit': self.a2c_network.eval_disc(input_dict['normalized_replay_disc_obs']),
-        #             'demo_disc_logit': self.a2c_network.eval_disc(input_dict['normalized_demo_disc_obs']),
-        #             'enc': self.a2c_network.eval_enc(input_dict['normalized_rollout_disc_obs']),
+        #             'rollout_disc_logit': self.disc(input_dict['normalized_rollout_disc_obs']),
+        #             'replay_disc_logit': self.disc(input_dict['normalized_replay_disc_obs']),
+        #             'demo_disc_logit': self.disc(input_dict['normalized_demo_disc_obs']),
+        #             'enc': self.enc(input_dict['normalized_rollout_disc_obs']),
         #         })
         #     else:
         #         selected_action = distr.sample()
@@ -90,36 +53,72 @@ class SkillModel(StyleModel):
         #         })
         #     return result
 
-        def attach_latent(self, normed_obs: torch.Tensor, latent: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-            latent_feature = self.a2c_network.latent_feature(latent)
-            normalized_obs_actor = torch.cat([normed_obs, latent_feature], dim=-1)
-            normalized_obs_critic = torch.cat([normed_obs, latent], dim=-1)
-            return normalized_obs_actor, normalized_obs_critic
+        def forward(self, input_dict):
+            normed_obs = input_dict['obs']
+            ase_latents = input_dict['latent']
+            use_hidden_latents = input_dict.get('use_hidden_latents', True)
 
-        def actor_latent(self, normed_obs, latent):
-            act_obs, _ = self.attach_latent(normed_obs, latent)
-            mu, logstd = self.actor_module(act_obs)
+            mu, logstd = self.a2c_network.eval_actor(normed_obs, ase_latents, use_hidden_latents)
             sigma = torch.exp(logstd)
-            return mu, sigma
+            distr = torch.distributions.Normal(mu, sigma, validate_args=False)
+            normalized_value = self.a2c_network.eval_critic(normed_obs, ase_latents, use_hidden_latents)
 
-        def critic_latent(self, normed_obs, latent):
-            _, crt_obs = self.attach_latent(normed_obs, latent)
-            normalized_value = self.critic_module(crt_obs)
-            value = self.denorm_value(normalized_value)
-            return value
+            result = {
+                'mus': mu,
+                'sigmas': sigma,
+                'rnn_states': None,
+            }
 
+            if input_dict['is_train']:
+                prev_neglogp = self.neglogp(input_dict['prev_actions'], mu, sigma, logstd)
+                result.update({
+                    'prev_neglogp': torch.squeeze(prev_neglogp),
+                    'entropy': distr.entropy().sum(dim=-1),
+                    'values': normalized_value,
+                    'rollout_disc_logit': self.a2c_network.disc(input_dict['normalized_rollout_disc_obs']),
+                    'replay_disc_logit': self.a2c_network.disc(input_dict['normalized_replay_disc_obs']),
+                    'demo_disc_logit': self.a2c_network.disc(input_dict['normalized_demo_disc_obs']),
+                    'enc': self.a2c_network.enc(input_dict['normalized_rollout_disc_obs']),
+                })
+            else:
+                selected_action = distr.sample()
+                neglogp = self.neglogp(selected_action, mu, sigma, logstd)
+                result.update({
+                    'neglogpacs': torch.squeeze(neglogp),
+                    'actions': selected_action,
+                    'values': self.denorm_value(normalized_value),
+                })
+            return result
+
+        # def attach_latent(self, normed_obs: torch.Tensor, latent: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        #     latent_feature = self.a2c_network.latent_feature(latent)
+        #     normalized_obs_actor = torch.cat([normed_obs, latent_feature], dim=-1)
+        #     normalized_obs_critic = torch.cat([normed_obs, latent], dim=-1)
+        #     return normalized_obs_actor, normalized_obs_critic
+        #
         # def actor_latent(self, normed_obs, latent):
-        #     mu, logstd = self.a2c_network.eval_actor(normed_obs, latent)
+        #     act_obs, _ = self.attach_latent(normed_obs, latent)
+        #     mu, logstd = self.actor_module(act_obs)
         #     sigma = torch.exp(logstd)
         #     return mu, sigma
         #
         # def critic_latent(self, normed_obs, latent):
-        #     normalized_value = self.a2c_network.eval_critic(normed_obs, latent)
+        #     _, crt_obs = self.attach_latent(normed_obs, latent)
+        #     normalized_value = self.critic_module(crt_obs)
         #     value = self.denorm_value(normalized_value)
         #     return value
 
+        def actor_latent(self, normed_obs, latent):
+            mu, logstd = self.a2c_network.eval_actor(normed_obs, latent)
+            sigma = torch.exp(logstd)
+            return mu, sigma
+
+        def critic_latent(self, normed_obs, latent):
+            normalized_value = self.a2c_network.eval_critic(normed_obs, latent)
+            value = self.denorm_value(normalized_value)
+            return value
+
         def enc(self, normalized_obs):
-            # return self.a2c_network.eval_enc(normalized_obs)
             return self.a2c_network.enc(normalized_obs)
 
         def enc_load_state_dict(self, state_dict):
