df85fc774be44224e3f83ae0833a4244ab39cd1f
daebangstn <gunho1107@naver.com>
2024-05-16 21:18:07 +0900

    [bugfix] ckpt converter, redundant keys

--- scripts/convert/convert_ase_pth.py
+++ scripts/convert/convert_ase_pth.py
@@ -2,7 +2,7 @@ from copy import deepcopy as dc
 import torch
 
 
-ase_pth_path = 'runs/Humanoid_00008100.pth'
+ase_pth_path = 'runs/Humanoid_ep_18500_rew_1155.2125_18500.pth'
 
 ase_pth = torch.load(ase_pth_path)
 model = ase_pth['model']
@@ -13,6 +13,12 @@ model["a2c_network.actor_mlp.2.weight"] = dc(model["a2c_network.actor_mlp._dense
 model["a2c_network.actor_mlp.2.bias"] = dc(model["a2c_network.actor_mlp._dense_layers.1.bias"])
 model["a2c_network.actor_mlp.4.weight"] = dc(model["a2c_network.actor_mlp._dense_layers.2.weight"])
 model["a2c_network.actor_mlp.4.bias"] = dc(model["a2c_network.actor_mlp._dense_layers.2.bias"])
+del model["a2c_network.actor_mlp._dense_layers.0.weight"]
+del model["a2c_network.actor_mlp._dense_layers.0.bias"]
+del model["a2c_network.actor_mlp._dense_layers.1.weight"]
+del model["a2c_network.actor_mlp._dense_layers.1.bias"]
+del model["a2c_network.actor_mlp._dense_layers.2.weight"]
+del model["a2c_network.actor_mlp._dense_layers.2.bias"]
 
 model["a2c_network.critic_mlp.0.weight"] = dc(model["a2c_network.critic_mlp._mlp.0.weight"])
 model["a2c_network.critic_mlp.0.bias"] = dc(model["a2c_network.critic_mlp._mlp.0.bias"])
@@ -20,6 +26,12 @@ model["a2c_network.critic_mlp.2.weight"] = dc(model["a2c_network.critic_mlp._mlp
 model["a2c_network.critic_mlp.2.bias"] = dc(model["a2c_network.critic_mlp._mlp.2.bias"])
 model["a2c_network.critic_mlp.4.weight"] = dc(model["a2c_network.critic_mlp._mlp.4.weight"])
 model["a2c_network.critic_mlp.4.bias"] = dc(model["a2c_network.critic_mlp._mlp.4.bias"])
+del model["a2c_network.critic_mlp._mlp.0.weight"]
+del model["a2c_network.critic_mlp._mlp.0.bias"]
+del model["a2c_network.critic_mlp._mlp.2.weight"]
+del model["a2c_network.critic_mlp._mlp.2.bias"]
+del model["a2c_network.critic_mlp._mlp.4.weight"]
+del model["a2c_network.critic_mlp._mlp.4.bias"]
 
 model["a2c_network._latent_enc_mlp.0.weight"] = dc(model["a2c_network.actor_mlp._style_mlp.0.weight"])
 model["a2c_network._latent_enc_mlp.0.bias"] = dc(model["a2c_network.actor_mlp._style_mlp.0.bias"])
@@ -27,29 +39,22 @@ model["a2c_network._latent_enc_mlp.2.weight"] = dc(model["a2c_network.actor_mlp.
 model["a2c_network._latent_enc_mlp.2.bias"] = dc(model["a2c_network.actor_mlp._style_mlp.2.bias"])
 model["a2c_network._latent_enc_mlp.4.weight"] = dc(model["a2c_network.actor_mlp._style_dense.weight"])
 model["a2c_network._latent_enc_mlp.4.bias"] = dc(model["a2c_network.actor_mlp._style_dense.bias"])
+del model["a2c_network.actor_mlp._style_mlp.0.weight"]
+del model["a2c_network.actor_mlp._style_mlp.0.bias"]
+del model["a2c_network.actor_mlp._style_mlp.2.weight"]
+del model["a2c_network.actor_mlp._style_mlp.2.bias"]
+del model["a2c_network.actor_mlp._style_dense.weight"]
+del model["a2c_network.actor_mlp._style_dense.bias"]
 
-model["a2c_network._disc.0.0.weight"] = dc(model["a2c_network._enc_mlp.0.weight"])
-model["a2c_network._disc.0.0.bias"] = dc(model["a2c_network._enc_mlp.0.bias"])
-model["a2c_network._enc.0.0.weight"] = dc(model["a2c_network._enc_mlp.0.weight"])
-model["a2c_network._enc.0.0.bias"] = dc(model["a2c_network._enc_mlp.0.bias"])
-model["a2c_network._disc.0.2.weight"] = dc(model["a2c_network._enc_mlp.2.weight"])
-model["a2c_network._disc.0.2.bias"] = dc(model["a2c_network._enc_mlp.2.bias"])
-model["a2c_network._enc.0.2.weight"] = dc(model["a2c_network._enc_mlp.2.weight"])
-model["a2c_network._enc.0.2.bias"] = dc(model["a2c_network._enc_mlp.2.bias"])
-model["a2c_network._disc.0.4.weight"] = dc(model["a2c_network._enc_mlp.4.weight"])
-model["a2c_network._disc.0.4.bias"] = dc(model["a2c_network._enc_mlp.4.bias"])
-model["a2c_network._enc.0.4.weight"] = dc(model["a2c_network._enc_mlp.4.weight"])
-model["a2c_network._enc.0.4.bias"] = dc(model["a2c_network._enc_mlp.4.bias"])
-
-model["a2c_network._disc.1.weight"] = dc(model["a2c_network._disc_logits.weight"])
-model["a2c_network._disc.1.bias"] = dc(model["a2c_network._disc_logits.bias"])
 model["a2c_network._disc_logistics.weight"] = dc(model["a2c_network._disc_logits.weight"])
 model["a2c_network._disc_logistics.bias"] = dc(model["a2c_network._disc_logits.bias"])
+del model["a2c_network._disc_logits.weight"]
+del model["a2c_network._disc_logits.bias"]
 
-model["a2c_network._enc.1.weight"] = dc(model["a2c_network._enc.weight"])
-model["a2c_network._enc.1.bias"] = dc(model["a2c_network._enc.bias"])
 model["a2c_network._enc_linear.weight"] = dc(model["a2c_network._enc.weight"])
 model["a2c_network._enc_linear.bias"] = dc(model["a2c_network._enc.bias"])
+del model["a2c_network._enc.weight"]
+del model["a2c_network._enc.bias"]
 
 model['running_mean_std.running_mean'] = dc(ase_pth['running_mean_std']['running_mean'])
 model['running_mean_std.running_var'] = dc(ase_pth['running_mean_std']['running_var'])
